df045694890db7359bb772574f6130e9fb94eba3
sirius.dos.santos <sirius.dos.santos@ibm.com>
2019-06-16 21:01:00 -0300

    Adicionar arquivos de projeto.

---
+++ ArcticFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArcticFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
